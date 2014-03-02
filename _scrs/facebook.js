a823abbed431a352d19e7c8740d0a1ac013593e4
Sam Acuna <samueleishion@gmail.com>
2014-03-02 08:28:45 -0600

    Added intro section and started using the Facebook API

---
+++ _scrs/facebook.js
@@ -0,0 +1,15 @@
+$(document).ready(function() { 
+	get_event_image(); 
+}); 
+
+function get_event_image() {
+	$.ajax({
+		type:'GET', 
+		url:'http://graph.facebook.com/ELAatUTCS', 
+		success: function(data) {
+			var img = data["cover"]["source"]; 
+			$('section.activities').append('<img src="'+img+'">'); 
+			console.log(img); 
+		}
+	}); 
+}
