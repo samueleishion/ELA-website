4c76388a494bba5392564ed54e5370e7ea6d2c90
Sam Acuna <samueleishion@gmail.com>
2014-03-23 17:26:08 -0500

    Improved Facebook API to get ELA page data

--- _scrs/facebook.js
+++ _scrs/facebook.js
@@ -1,6 +1,6 @@
 $(document).ready(function() { 
 	get_event_image(); 
-	get_page_info(); 
+	get_page_albums(); 
 }); 
 
 function get_event_image() {
@@ -9,7 +9,7 @@ function get_event_image() {
 		url:'http://graph.facebook.com/ELAatUTCS', 
 		success: function(data) {
 			var img = data["cover"]["source"]; 
-			$('section.activities').append('<img src="'+img+'">'); 
+			$('section.activities content').append('<img src="'+img+'">'); 
 		}
 	}); 
 }
@@ -20,4 +20,45 @@ function get_page_info() {
 			console.log(data); 
 		});
 	});  
+}
+
+function get_page_albums() {
+	$.getScript('//connect.facebook.net/en_US/all.js',function() {
+		FB.api('/ELAatUTCS/albums',function(data) {
+			var album, id, url, name; 
+
+			for(var i = 0; i < data["data"].length; i++) {
+				album = data["data"][i]; 
+				name = album["name"]; 
+				if(!valid_album_name(name)) continue; 
+				id = album["id"]; 
+				url = album["link"]; 
+				get_album_picture(id); 
+				get_album_photos(id); 
+			}
+		});
+	});  
+}
+
+function valid_album_name(album_name) {
+	return !((album_name=="Cover Photos") || (album_name=="Profile Pictures") || (album_name=="Timeline Photos")); 
+}
+
+function get_album_picture(album_id) { 
+	$.getScript('//connect.facebook.net/en_US/all.js',function() {
+		FB.api('/'+album_id+'/picture',function(data) {
+			var img = data["data"]["url"]; 
+		}); 
+	}); 
+}
+
+function get_album_photos(album_id) {
+	$.getScript('//connect.facebook.net/en_US/all.js',function() {
+		FB.api('/'+album_id+'/photos',function(data) {
+			var img; 
+			for(var i = 0; i < data["data"].length; i++) {
+				img = data["data"][i]["images"]; 
+			} 
+		}); 
+	}); 
 }
